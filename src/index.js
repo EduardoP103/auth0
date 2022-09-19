3712af2b0fc6bf58f40c9ae07bd6b04a66534f87
Eduardo Pastor <eduardopastor103@gmail.com>
2022-09-19 17:02:52 -0500

    example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider domain="eduardopastor.eu.auth0.com" 
+    clientId='aq1aTQPIbLQkxoWeE8s11AnoBDXLZLU8' 
+    redirectUri={window.location.origin}>
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
